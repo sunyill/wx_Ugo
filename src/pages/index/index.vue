a11d6632aff60dd0d73cf69406572883cbf5b062
sunyill <sunyill0707@163.com>
2019-09-27 15:00:26 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Description: 
+ * @Author: your name
+ * @Date: 2019-09-27 10:39:02
+ * @LastEditTime: 2019-09-27 14:56:41
+ * @LastEditors: Please set LastEditors
+ -->
+<template>
+  <div >
+
+   
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+</style>
